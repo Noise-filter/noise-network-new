ecbf7deef47ee81f627235f10ec29ed6bb6af407
noise-filter <pontus.fransson92@gmail.com>
2021-04-07 22:08:21 +0200

    Initial

---
+++ noise-network/src/library.cpp
@@ -0,0 +1,16 @@
+#include "library.h"
+
+#include <iostream>
+
+int main() {
+    hello();
+    return 0;
+}
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+    int *a;
+    return;
+}
+
+int add(int x, int y) { return x + y; }
---
+++ noise-network/src/library.h
@@ -0,0 +1,8 @@
+#ifndef NOISE_NETWORK_NEW_LIBRARY_H
+#define NOISE_NETWORK_NEW_LIBRARY_H
+
+void hello();
+
+int add(int x, int y);
+
+#endif  // NOISE_NETWORK_NEW_LIBRARY_H
